bb7dea1197f27b109de304ebe54b11c8e6179421
s60127h <s60127h@gmail.com>
2019-02-18 04:08:10 +0800

    fix dessert page image's setting

--- dessert.php
+++ dessert.php
@@ -9,75 +9,95 @@
     <?php include("nav.php") ?>
     <!-- 要多給圖片一個div不然margin-top不對會跑版 -->
 	<div class="main-area clear">
+    <br>
+    <h2 class="dessert-title">甜點清單</h2>
+    <hr class="hr-style">
       <ul id="gallery">
         <li>
             <a href="#" class="wrapper">
                 <img src="images/dessert/雪Q餅.jpg">
+                <p class="dessert-name">雪Q餅</p>
             </a>
+            
         </li>
         <li>
             <a href="#" class="wrapper">
                 <img src="images/dessert/手工餅乾.jpg">
-            </a> 
+                <p class="dessert-name">手工餅乾</p> 
+            </a>
+
         </li>
         <li>
             <a href="#" class="wrapper">
                 <img src="images/dessert/香蕉巧克力布朗尼.jpg">
+                <p class="dessert-name">香蕉巧克力布朗尼</p>
             </a> 
+            
         </li>
         <li>
             <a href="#" class="wrapper">
                 <img src="images/dessert/奶酒oreo生乳酪.jpg">
+                <p class="dessert-name">奶酒oreo生乳酪</p>
             </a> 
         </li>
         <li>
             <a href="#" class="wrapper">
                 <img src="images/dessert/法式吐司.jpg">
+                <p class="dessert-name">法式吐司</p>
             </a> 
         </li>
         <li>
             <a href="#" class="wrapper">
                 <img src="images/dessert/抹茶紅豆千層.jpg">
+                <p class="dessert-name">抹茶紅豆千層</p>
             </a> 
         </li>
         <li>
             <a href="#" class="wrapper">
                 <img src="images/dessert/三眼怪抹茶塔.jpg">
+                <p class="dessert-name">三眼怪抹茶塔</p>
             </a> 
         </li>
         <li>
             <a href="#" class="wrapper">
                 <img src="images/dessert/牛粒.jpg">
+                <p class="dessert-name">牛粒</p>
             </a> 
         </li>
         <li>
             <a href="#" class="wrapper">
                 <img src="images/dessert/台式蛋塔.jpg">
+                <p class="dessert-name">台式蛋塔</p>
             </a> 
         </li>
         <li>
             <a href="#" class="wrapper">
                 <img src="images/dessert/原味日式起司條.jpg">
+                <p class="dessert-name">原味日式起司條</p>
             </a> 
         </li>
         <li>
             <a href="#" class="wrapper">
                 <img src="images/dessert/鬆餅.jpg">
+                <p class="dessert-name">鬆餅</p>
             </a> 
         </li>
         <li>
             <a href="#" class="wrapper">
                 <img src="images/dessert/輕乳酪蛋糕.jpg">
+                <p class="dessert-name">輕乳酪蛋糕</p>
             </a> 
         </li>
         <li>
             <a href="#" class="wrapper">
                 <img src="images/dessert/生巧克力塔.jpg">
+                <p class="dessert-name">生巧克力塔</p>
             </a> 
         </li>
         <li>
             <a href="#" class="wrapper">
                 <img src="images/dessert/三明治.jpg">
+                <p class="dessert-name">三明治</p>
             </a> 
         </li>
       </ul>
