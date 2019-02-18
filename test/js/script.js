e7244006c99ee7b42e7d06663108202d1f2b6206
s60127h <s60127h@gmail.com>
2019-02-19 04:18:19 +0800

    practice jquery slide

--- test/js/script.js
+++ test/js/script.js
@@ -2,4 +2,21 @@ $(document).ready(function(){
 	$('#name').change(function(){
 		$('.bind-data').html($(this).val());
 	});
+
+	$('#number').change(function(){
+		$('.bind-number').append($(this).val());
+	});
+
+/*	$('.slideup').click(function(){
+		$('.slidewords').slideUp(200);
+	});
+
+	$('.slidedown').click(function(){
+		$('.slidewords').slideDown(200);
+	});
+*/
+
+	$('.slidetoggle').click(function(){
+		$('.slidewords').slideToggle(200);
+	});
 });
