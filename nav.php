fe795694bb987335abb09c4e0b027b5a9b183307
s60127h <s60127h@gmail.com>
2019-02-20 00:16:29 +0800

    add icon to nav bar

--- nav.php
+++ nav.php
@@ -1,14 +1,23 @@
 
     <div class="menu-container">
-      <div class="menu-logo">
+      <div class="menu-logo clear">
         <h1><a href="index.php">My Dessert</a></h1>
       </div><!-- menu-logo -->
-      <nav class="site-nav">
-        <ul class="clear">
-          <li><a href="dessert.php">手作甜點</a></li>
-          <li><a href="restaurant.php">餐廳推薦</a></li>
-          <li><a href="photo.php">攝影景點</a></li>
-          <li><a href="work.php">作品集</a></li>
-        </ul>
+      <nav class="nav">
+        <div class="nav-menu">
+          <ul class="clear">
+            <li><a href="dessert.php">手作甜點</a></li>
+            <li><a href="restaurant.php">餐廳推薦</a></li>
+            <li><a href="photo.php">攝影景點</a></li>
+            <li><a href="work.php">作品集</a></li>
+          </ul>
+        </div>
+        <div class="nav-contact">
+          <ul class="clear">
+            <li><a href="https://www.instagram.com/sr_dessertlife/"><img src="images/icon/instagram.png"></a></li>
+            <li><a href="https://github.com/s60127h/dessert"><img src="images/icon/github.png"></a></li>
+            <li><a href="mail.php"><img src="images/icon/gmail.png"></a></li>
+          </ul>
+        </div>
       </nav>
     </div><!-- menu-container -->
